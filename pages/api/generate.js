e572a01755295c64015e9c9b489ac45b3c0537dc
usmaneth <97273722+usmaneth@users.noreply.github.com>
2023-01-03 11:25:54 -0800

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,8 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.INPUT_OPENAI_API_KEY || process.env.OPENAI_API_KEY,
+
 });
 
 const openai = new OpenAIApi(configuration);
