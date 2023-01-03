1b5c6451a0ca1c526ec352b86797c07dff2f5a2b
usmaneth <usmanasim27@gmail.com>
2023-01-02 22:52:23 -0800

    First Commit

---
+++ pages/api/generate.js
@@ -0,0 +1,40 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+const basePromptPrefix = "";
+const generateAction = async (req, res) => {
+
+    const basePromptPrefix = 
+    `provide an authentic hadith  (from sources: Sahih al-Bukhari, Sahih Muslim)that fits in response to the prompt the user gives. be sure to provide the hadith from the following format - 
+    (Bold out each section nicely)
+    Specific Reference: 
+
+    Hadith (Original Arabic): 
+
+    Hadith (English Interpretation): 
+
+    (Be sure to discuss the implementation in great detail with specific exammples)
+    How to Implement (In Relation to the prompt):`;
+
+  // Run first prompt
+  console.log(`API:${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 1000,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,98 @@ import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
+import { useState } from 'react';
+
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+ 
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+  
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+  
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+  
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
+
+
+  const onUserChangedText = (event) => {
+    (event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Pocket Hadith</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Find peace, comfort, and direction through the Hadith of the Prophet SAW & discover the perfect Hadith for every emotion and situation </h2>
+
+            <h2>Type in whatever you are feeling/going through and the AI will find the perfect hadith to match the situation</h2>
+
+            <h3> Disclaimer: This machine learning bot is trained on only two sources (Sahih al-Bukhari & Sahih Muslim) More can be added later inshaAllah </h3>
           </div>
         </div>
+  
+        <div className="prompt-container">
+        <textarea
+          className="prompt-box"
+          placeholder="start typing here"
+          value= {userInput}
+          onChange={onUserChangedText}
+        />;
+        <div className="prompt-buttons">
+        <a className={isGenerating ? 'generate-button loading' : 'generate-button'}
+         onClick={callGenerateEndpoint}>
+        <div className="generate">
+    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+    </div>
+  </a>
+        </div>
+        {apiOutput && (
+            <div className="output">
+            <div className="output-header-container">
+            <div className="output-header">
+            <h3>Hadith</h3>
+          </div>
       </div>
+          <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+        </div>
+      </div>
+     
+     
+     
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="https://buildspace.so/@usman"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>Built by Usman Asim</p>
           </div>
         </a>
       </div>
